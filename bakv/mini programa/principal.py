a7c49dd3d96a899bbf6d9cda3fe6c31f440f99f9
Pavivinola <jorge.moreno33@inacapmail.cl>
2025-08-26 22:21:34 -0400

    fadf

--- bakv/mini programa/principal.py
+++ bakv/mini programa/principal.py
@@ -10,6 +10,9 @@ venta =  Venta()
 vegetariano = SandwichVegetariano("integral", 2000, "tomate, lechuga")
 carne = SandwichCarne("centeno", 2500, "cheddar")
 
+
+#setear variable
+instanciarVar = carne.set_precio(3200)
 #agregar pedidos
 
 venta.agregar_sandwich(vegetariano)
